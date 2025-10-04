51928fd0bfab174e01272217358fe41e41636209
sanjay-yadav-05 <sy163182@gmail.com>
2025-10-05 00:20:37 +0530

    payment feature check completely

--- backend/src/modules/payments/payments.resolver.ts
+++ backend/src/modules/payments/payments.resolver.ts
@@ -3,16 +3,22 @@ import { UseGuards } from '@nestjs/common';
 import { Payment } from './entities/payment.entity';
 import { PaymentsService } from './payments.service';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { InitiatePaymentResponseDto } from './dto/payment.input';
 
 @Resolver(() => Payment)
 export class PaymentsResolver {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Mutation(() => String)
+  // @Mutation(() => String)
+  @Mutation(() => InitiatePaymentResponseDto)
   @UseGuards(JwtAuthGuard)
-  async initiatePayment(@Args('bookingId') bookingId: string): Promise<any> {
+  async initiatePayment(@Args('bookingId') bookingId: string): Promise<InitiatePaymentResponseDto> {
     return this.paymentsService.initiatePayment(bookingId);
   }
+  // @UseGuards(JwtAuthGuard)
+  // async initiatePayment(@Args('bookingId') bookingId: string): Promise<any> {
+  //   return this.paymentsService.initiatePayment(bookingId);
+  // }
 
   @Mutation(() => Boolean)
   @UseGuards(JwtAuthGuard)
